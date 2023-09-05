8d6cd774c4d02cf738ef7ee070e46ae8a4055f61
carillo356 <107926755+carillo356@users.noreply.github.com>
2023-09-05 09:15:52 +0800

    first commit

---
+++ src/main/java/com/gmpc/notesonline/NotesOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.gmpc.notesonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmpc/notesonline/NotesOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmpc.notesonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
